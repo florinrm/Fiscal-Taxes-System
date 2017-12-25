3c747a420ebb62d3d0092b7faf8f74a474b5c765
Florin Mihalache <florinrm@users.noreply.github.com>
2017-12-25 16:26:28 +0200

    Adding some GUI (login form for now)

--- src/tax_system/AppStart.java
+++ src/tax_system/AppStart.java
@@ -3,23 +3,176 @@ package tax_system;
 import javax.swing.*;
 import java.awt.*;
 
-public class AppStart extends JFrame {
-    public JTextField user;
-    public JPanel login;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    public AppStart (String text)
-    {
-        super(text);
-        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        setMinimumSize (new Dimension(800, 450));
-        getContentPane ().setBackground(Color.blue);
-        setLayout (new SpringLayout ());
-        this.user = new JTextField(15);
-        this.login = new JPanel(new SpringLayout());
-        this.login.setBounds(5, 5, 290, 290);
-        this.login.add(user);
-        add(this.login);
-        setVisible(true);
-        pack();
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AppStart extends JFrame {
+    private JTextField user;
+    private JPanel panel;
+    private JPasswordField pass;
+    private JButton login_button;
+    private JLabel username;
+    private JLabel password;
+    private JLabel succeded;
+    private JButton create_account;
+    private JPanel buttons_panel;
+
+    public void insert_data (String username, String password) {
+        File data_file = new File("login.txt");
+        HashMap <String, String> map = new HashMap<>();
+        if (!data_file.exists())
+            try {
+                data_file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        Scanner scan = null;
+        try {
+            scan = new Scanner(data_file);
+            String line;
+            while (scan.hasNextLine()) {
+                line = scan.nextLine();
+                String[] data = line.split (" ");
+                if (username.equals(data[0]))
+                {
+                    succeded.setText("Account already exists");
+                    return;
+                }
+                map.put(data[0], data[1]);
+            }
+            scan.close();
+            map.put(username, password);
+            System.setOut(new PrintStream(data_file));
+            for (Map.Entry<String, String> entry: map.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+            succeded.setText("Account created!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void login_account (String username, String password) {
+        File data_file = new File ("login.txt");
+        // HashMap <String, String> map = new HashMap<>();
+        if (!data_file.exists())
+        {
+            succeded.setText("Account doesn't exist!");
+            return;
+        }
+        Scanner scan = null;
+        try {
+            scan = new Scanner (data_file);
+            String line;
+            while (scan.hasNextLine()) {
+                line = scan.nextLine();
+                String[] data = line.split(" ");
+                if (username.equals(data[0])) {
+                    if (password.equals(data[1])) {
+                        succeded.setText("Logged in succesfully!");
+                        new Main();
+                        setVisible(false); //you can't see me!
+                        dispose();
+                        return;
+                    } else {
+                        succeded.setText("Wrong password!");
+                        return;
+                    }
+                }
+            }
+            scan.close();
+            succeded.setText("Account doesn't exist!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public AppStart () {
+        super ("Welcome");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBackground(Color.BLUE);
+        this.setMinimumSize(new Dimension(400, 200));
+        this.setLayout(new FlowLayout());
+
+        this.user = new JTextField(25);
+        this.panel = new JPanel();
+        this.pass = new JPasswordField(25);
+        this.username = new JLabel("Username", JLabel.CENTER);
+        this.password = new JLabel("Password");
+        this.login_button = new JButton ("Login");
+        this.create_account = new JButton("Create new account");
+        this.succeded = new JLabel();
+        this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.Y_AXIS));
+        //this.panel.setBackground(Color.BLUE);
+        this.create_account.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton button = (JButton) e.getSource();
+                if (button.getText().equals("Create new account")) {
+                    insert_data(user.getText(), pass.getText());
+                }
+            }
+        });
+        this.login_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton button = (JButton) e.getSource();
+                if (button.getText().equals("Login")) {
+                    login_account(user.getText(), pass.getText());
+                }
+            }
+        });
+        this.buttons_panel = new JPanel();
+        this.buttons_panel.setLayout(new FlowLayout());
+        this.panel.setMinimumSize(new Dimension (300, 400));
+        this.buttons_panel.add(this.login_button);
+        this.buttons_panel.add(this.create_account);
+        this.panel.add(this.username);
+        this.panel.add(this.user);
+        this.panel.add(this.password);
+        this.panel.add(this.pass);
+        this.panel.add(this.buttons_panel);
+        this.panel.add(this.succeded);
+        this.add(panel);
+        this.pack();
+        this.setResizable(true);
+        this.setVisible(true);
+    }
+
+    public static void main (String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        new AppStart();
     }
 }
+
--- src/tax_system/Main.java
+++ src/tax_system/Main.java
@@ -4,6 +4,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    public Main () {
+        FileParsing parsing = new FileParsing();
+        ArrayList <Produs> list = parsing.getListProdus("produse.txt");
+        ArrayList <TaxaProdus> list2 = parsing.getTaxe("taxe.txt");
+        ArrayList <Magazin> list3 = parsing.getMagazine("facturi.txt", list, list2);
+
+
+        TreeSet <String> countries = parsing.tariOrigine;
+        TreeMap <String, HashMap<String, Double>> map = new TreeMap<>();
+        Iterator iterate = countries.iterator();
+        while (iterate.hasNext()) {
+            String country = iterate.next().toString();
+            HashMap<String, Double> element = new HashMap<>();
+            for (int i = 0; i < list2.size(); ++i) {
+                if (country.equals(list2.get(i).getTaraOrigine()))
+                    element.put(list2.get(i).getDenumire(), list2.get(i).getTaxa());
+            }
+            map.put(country, element);
+        }
+        Gestiune obj = Gestiune.getInstance();
+        obj.setMagazine(list3);
+        obj.setProduse(list);
+        obj.setTaxes(map);
+
+
+        TreeSet<String> tipuriMagazine = parsing.tipuriMagazine;
+        Iterator iter = tipuriMagazine.iterator();
+        Collections.sort(list3);
+        try {
+            System.setOut(new PrintStream(new File("output.txt")));
+            while (iter.hasNext()) {
+                String type = iter.next().toString();
+                System.out.println(type);
+                for (int i = 0; i < list3.size(); ++i) {
+                    // parsam tipul magazinului (in interiorul clasei imi erau aruncate exceptii cand faceam parsarea
+                    // asa ca o fac aici
+                    if (type.equals(list3.get(i).type.split(" ")[1].split("_")[1].substring(7)))
+                        System.out.println(list3.get(i));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
     public static void main (String[] args)
     {
         FileParsing parsing = new FileParsing();
@@ -29,7 +76,7 @@ public class Main {
         obj.setProduse(list);
         obj.setTaxes(map);
 
-        
+
         TreeSet<String> tipuriMagazine = parsing.tipuriMagazine;
         Iterator iter = tipuriMagazine.iterator();
         Collections.sort(list3);
@@ -39,6 +86,8 @@ public class Main {
                 String type = iter.next().toString();
                 System.out.println(type);
                 for (int i = 0; i < list3.size(); ++i) {
+                    // parsam tipul magazinului (in interiorul clasei imi erau aruncate exceptii cand faceam parsarea
+                    // asa ca o fac aici
                     if (type.equals(list3.get(i).type.split(" ")[1].split("_")[1].substring(7)))
                         System.out.println(list3.get(i));
                 }
@@ -47,6 +96,5 @@ public class Main {
             e.printStackTrace();
         }
 
-
-    }
+    } */
 }
